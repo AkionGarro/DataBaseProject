37a37a6eb76bb03b51baf32a0d95b39c6731e501
Jonathan Corrales Jimenez <jona.corrales.97@gmail.com>
2022-01-08 19:15:15 -0600

    Update APPUSER.sql

--- APPUSER.sql
+++ APPUSER.sql
@@ -3,7 +3,7 @@ create table APPUSER
 (
 idAppUser number(8),
 username varchar2(10),
-identificationNumber VARCHAR2(10) constraint appuser_identification_nn not null,
+idPeople number(8),
 userType Number(2) constraint appuser_userType_nn not null,
 district Number(10) constraint appuser_disctrict_nn not null,
 password varchar2(10) constraint user_password_nn not null,
@@ -30,8 +30,8 @@ STORAGE (INITIAL 10K NEXT 10K PCTINCREASE 0);
 -----------------------------------Foreign Keys------------------------------------
 
 ALTER TABLE APPUSER
-ADD CONSTRAINT fk_APPUSER_persona FOREIGN KEY
-(identificationNumber) REFERENCES PEOPLE(identificationNumber);
+ADD CONSTRAINT fk_APPUSER_people FOREIGN KEY
+(idPeople) REFERENCES PEOPLE(idPeople);
 
 ALTER TABLE APPUSER
 ADD CONSTRAINT fk_APPUSER_usertpe FOREIGN KEY
@@ -54,7 +54,7 @@ IS 'Unique identifier for appuser tuple';
 COMMENT ON COLUMN APPUSER.username
 IS 'The username that identifies the user';
 
-COMMENT ON COLUMN APPUSER.identificationNumber
+COMMENT ON COLUMN APPUSER.idPeople
 IS 'Reference to the identification from the people ';
 
 COMMENT ON COLUMN APPUSER.userType
