ae4ef2ca302a83c31e55f8f288e76952261ed907
Carlos Akión Garro Campos <carloscamp1008@estudianTEC.cr>
2022-01-12 15:00:40 -0800

    titles

--- ClockSocialNetwork/src/com/View/adminPanels/ClockType.java
+++ ClockSocialNetwork/src/com/View/adminPanels/ClockType.java
@@ -26,11 +26,28 @@ public class ClockType extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        titleBrand = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+
         setBackground(new java.awt.Color(255, 255, 255));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        titleBrand.setBackground(new java.awt.Color(0, 0, 102));
+        titleBrand.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setBackground(new java.awt.Color(0, 0, 102));
+        jLabel1.setFont(new java.awt.Font("Roboto", 1, 18)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("New Clock Type");
+        jLabel1.setToolTipText("");
+        titleBrand.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 630, 50));
+
+        add(titleBrand, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 630, 50));
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel titleBrand;
     // End of variables declaration//GEN-END:variables
 }
