dd07f0396d19d7f7d43b6231b39352bf12e98a29
Jonathan Corrales Jimenez <jona.corrales.97@gmail.com>
2022-01-14 17:57:36 -0600

    Create fix14.1.sql

---
+++ Backend/fix14.1.sql
@@ -0,0 +1,8 @@
+drop table typeclockxbrand;
+delete from modelP;
+alter table modelp
+
+add idTypeClock number(3) constraint modelP_idTypeClock_nn NOT NULL;
+
+ALTER TABLE modelP
+ADD CONSTRAINT fk_modelp_typeclock FOREIGN KEY (idTypeClock) REFERENCES typeClock(idtype);
