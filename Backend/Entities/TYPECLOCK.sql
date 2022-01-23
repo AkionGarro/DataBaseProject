8f1165ccd066660e635d9094157e432d21c6f558
Carlos Akión Garro Campos <carloscamp1008@estudianTEC.cr>
2022-01-23 12:21:30 -0600

    Update Directory

---
+++ Backend/Entities/TYPECLOCK.sql
@@ -0,0 +1,36 @@
+CREATE TABLE TYPECLOCK(
+idType number(3),
+nameType varchar(20) CONSTRAINT TYPECLOCK_nameType_nn not null,
+descriptionT varchar(100),
+createdBy VARCHAR2(20),
+creationDate DATE,
+modifiedBy VARCHAR2(20),
+modificationDate DATE
+);
+
+-----------------------------------PK------------------------------------------------------
+
+ALTER TABLE TYPECLOCK
+ADD CONSTRAINT pk_TYPECLOCK PRIMARY KEY(idType)
+USING INDEX
+TABLESPACE pr_ind PCTFREE 20
+STORAGE (INITIAL 10K NEXT 10K PCTINCREASE 0);
+----------------------------------Comments-------------------------------------------------------
+
+
+COMMENT ON TABLE TYPECLOCK
+is 'It is a table that has the types of clocks; for example: wall clock, cuckoo clock, watch, etc…';
+COMMENT ON COLUMN TYPECLOCK.idType
+is 'Unique identifier of the  type of clock';
+COMMENT ON COLUMN TYPECLOCK.nameType
+is 'Name of the type of clock';
+COMMENT ON COLUMN TYPECLOCK.descriptionT
+is 'Description of the type of clock';
+COMMENT ON COLUMN TYPECLOCK.createdBy
+is 'The user who created the tuple';
+COMMENT ON COLUMN TYPECLOCK.creationDate
+is 'The date when the tuple was created';
+COMMENT ON COLUMN TYPECLOCK.modifiedBy
+is 'The user who modified the tuple';
+COMMENT ON COLUMN TYPECLOCK.modificationDate
+is 'The date when the tuple was modified';
